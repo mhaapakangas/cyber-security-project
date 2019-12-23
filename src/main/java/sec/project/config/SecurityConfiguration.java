ce73864d9b955ee0a962d5d7dcb2b6a0351189af
Maija Haapakangas <mhaapakangas@gmail.com>
2019-12-23 20:28:29 +0200

    Misconfigure security

--- src/main/java/sec/project/config/SecurityConfiguration.java
+++ src/main/java/sec/project/config/SecurityConfiguration.java
@@ -20,6 +20,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionFixation().none();
+
         http.authorizeRequests()
             .anyRequest().authenticated();
         http.formLogin()
